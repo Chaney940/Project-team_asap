3433be477eb505d787a56400bd00ef828308f1f6
张辉 Zhang Hui <1601213658@sz.pku.edu.cn>
2017-11-04 23:28:09 +0800

    add code comment

--- option_models/basket_Levy.py
+++ option_models/basket_Levy.py
@@ -4,24 +4,45 @@ Created on Sat Nov  4 16:11:55 2017
 
 @author: lantm
 """
-
+# imports necessary modules (numpy and scipy.stats)
 import numpy as np
 import scipy.stats as ss
+
+'''
+define a function of calculation basket price using Levy's log-normal moment matching
+1.there is a lot of evidence suggesting that the distribution of Basket option can be 
+  well-approximated by another log normal distribution
+2.using moment matching method, if mean of basket option is M, variance is V*V-M*M, 
+  we can derive that the mean of the new lognormal distribution is m = 2 * log(M) - 0.5 * log(V*V),
+  and varance is v2 = log(V*V) - 2 * log(M)
+3.use the new parameter and black scholes model fomula to approximate calculate option price
+'''
 
 def basket_price_Levy(
     strike, spot, vol, weights, 
     texp, cor_m, intr=0.0, divr=0.0, cp_sign=1
 ):
+    #compute dividend factor, discount factor, forward price
     div_fac = np.exp(-texp*divr)
     disc_fac = np.exp(-texp*intr)
     forward = spot / disc_fac * div_fac
+    
+    #compute mean of basket option
     M = forward @ weights[:, None]
+    
+    #compute variance of basket option
     forward_w = forward * weights
     cov_m = vol * cor_m * vol[:,None]
     V2 = forward_w @ np.exp(cov_m * texp) @ forward_w
+    
+    #compute mean of log-normal distribution
     m = 2 * np.log(M) - 0.5 * np.log(V2)
+    
+    #compute variance and standard deviation of log-normal distribuion
     v2 = np.log(V2) - 2 * np.log(M)
     v = np.sqrt(v2)
+    
+    #using black scholes model calculate option price in the future
     d1 = (m - np.log(strike) + v2) / v
     d2 = d1 - v
     cp = (cp_sign - abs(cp_sign)) / 2
